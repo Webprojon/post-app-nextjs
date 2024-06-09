cc0977cfdc43a4d3b80f0f1edd59b5d930a60478
Webprojon <khasanovtokhir77@gmail.com>
2024-06-09 08:39:30 +0200

    first commit

---
+++ src/actions/action.tsx
@@ -0,0 +1,28 @@
+"use server";
+
+import prisma from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export async function createPost(formData: FormData) {
+	const { isAuthenticated } = getKindeServerSession();
+
+	if (!(await isAuthenticated())) {
+		redirect("/api/auth/login");
+	}
+
+	const title = formData.get("title") as string;
+	const body = formData.get("body") as string;
+
+	// Update database
+	await prisma.post.create({
+		data: {
+			title,
+			body,
+		},
+	});
+
+	// Revalidate
+	revalidatePath("/posts");
+}
---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<footer className="mt-auto text-center py-5 px-7 text-zinc-500 border-t">
+			<small>&copy; 2030. All rights reserved</small>
+		</footer>
+	);
+}
